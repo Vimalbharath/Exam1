94002b7ddb5845cf8b767fa0ba00352f9a7e134b
Vimalbharath <vimalbharath21@gmail.com>
2025-03-20 22:16:55 +0530

    small bug

---
+++ Arrays/Sort/selection.java
@@ -0,0 +1,38 @@
+package Arrays.Sort;
+
+import java.util.Arrays;
+
+public class selection {
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 1, 2, 0, 9, 8 };
+        System.out.println(Arrays.toString(arr));
+        selectionsort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void selectionsort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - 1 - i;
+            int max = maxindex(arr, 0, last);
+            swap(arr, max, last);
+        }
+    }
+
+    public static int maxindex(int[] arr, int first, int last) {
+        int max = first;
+        for (int i = 0; i < last; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
