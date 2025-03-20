a0ae4777ebc2c4b985c78cfee9fbd11a6e950414
Vimalbharath <vimalbharath21@gmail.com>
2025-03-20 14:11:39 +0530

    binary search code

--- Arrays/Search/binary.java
+++ Arrays/Search/binary.java
@@ -1,10 +1,28 @@
 package Arrays.Search;
 
-public class binary{
+public class binary {
 
-	public static void main(String [] args){
+	public static void main(String[] args) {
+		int[] arr = { 2, 4, 5, 6, 7, 8 };
+		int result = binarysearch(arr, 5);
+		System.out.println(result);
 
-		System.out.println("Hello World Edited in text editor" );
+	}
 
+	public static int binarysearch(int[] arr, int target) {
+		int s = 0;
+		int e = arr.length - 1;
+		while (s <= e) {
+			int m = s + (e - s) / 2;
+			if (arr[m] < target) {
+				s = m + 1;
+			} else if (arr[m] > target) {
+				e = m;
+			} else {
+				return m;
+			}
+
+		}
+		return -1;
 	}
 }
