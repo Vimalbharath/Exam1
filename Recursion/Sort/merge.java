7f812958f64b997ef0fd9080a3ea33b457fd0316
Vimalbharath <vimalbharath21@gmail.com>
2025-03-22 22:33:41 +0530

    infinte rec

---
+++ Recursion/Sort/merge.java
@@ -0,0 +1,53 @@
+package Recursion.Sort;
+
+import java.util.Arrays;
+
+public class merge {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 9, 7, 6, 2, 1 };
+        System.out.println(Arrays.toString(arr));
+        int[] result = mergesort(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] mergesort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int s = 0;
+        int e = arr.length - 1;
+        int m = s + (e - s) / 2;
+        int[] first = mergesort(Arrays.copyOfRange(arr, s, m));
+        int[] second = mergesort(Arrays.copyOfRange(arr, m + 1, e));
+        return combined(first, second);
+    }
+
+    public static int[] combined(int[] first, int[] second) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] mix = new int[i + j];
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+
+}
