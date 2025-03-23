a3992c65511791c7bf83243c4505ccf3418b6049
Vimalbharath <vimalbharath21@gmail.com>
2025-03-23 20:51:25 +0530

    is safe cross pending

--- Recursion/Backtracking/queens.java
+++ Recursion/Backtracking/queens.java
@@ -4,7 +4,7 @@ public class queens {
     public static void main(String[] args) {
         int n = 4;
         boolean[][] board = new boolean[n][n];
-        System.out.println(nqueens(board, 4));
+        System.out.println(nqueens(board, 0));
     }
 
     public static int nqueens(boolean[][] board, int r) {
@@ -12,7 +12,27 @@ public class queens {
             display(board);
             return 1;
         }
-        return 1;
+        int count = 0;
+        for (int c = 0; c < board.length; c++) {
+            if (isSafe(board, r, c)) {
+                board[r][c] = true;
+                count = count + nqueens(board, r + 1);
+                board[r][c] = false;
+            }
+        }
+        return count;
+    }
+
+    public static boolean isSafe(boolean[][] board, int r, int c) {
+        while (c > 0) {
+            if (board[r][c]) {
+
+                return false;
+            }
+
+            c--;
+        }
+        return true;
     }
 
     public static void display(boolean[][] board) {
