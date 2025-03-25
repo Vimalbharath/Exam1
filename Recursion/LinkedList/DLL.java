64172e721ea18804c515d2132a02c9aff6ef8965
Vimalbharath <vimalbharath21@gmail.com>
2025-03-25 11:44:51 +0530

    insert last and after good

--- Recursion/LinkedList/DLL.java
+++ Recursion/LinkedList/DLL.java
@@ -26,6 +26,48 @@ public class DLL {
         head = node;
     }
 
+    public void insertLast(int val) {
+        if (head == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        Node temp = head;
+        Node last = null;
+        while (temp != null) {
+            last = temp;
+            temp = temp.next;
+        }
+        last.next = node;
+        node.prev = last;
+
+    }
+
+    public void insertAfter(int after, int val) {
+        Node p = find(after);
+        if (p == null) {
+            System.out.println("Node doesn't exist");
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if (node.next != null)
+            node.next.prev = node;
+
+    }
+
+    public Node find(int val) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.val == val) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
     public void display() {
         Node temp = head;
         Node last = null;
@@ -48,6 +90,8 @@ public class DLL {
         ll.insertFirst(2);
         ll.insertFirst(3);
         ll.insertFirst(4);
+        ll.insertLast(1);
+        ll.insertAfter(1, 100);
         ll.display();
     }
 }
