26e5443cc2bbb8a5114b0403f0248ab1a472d64e
Vimalbharath <vimalbharath21@gmail.com>
2025-03-28 13:13:40 +0530

    looks tree is balacing, not seen code

--- Tree/AVL.java
+++ Tree/AVL.java
@@ -50,6 +50,58 @@ public class AVL {
 
     }
 
+    public Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // left heavy
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                // left left case
+                return rightRotate(node);
+            }
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                // left right case
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+        if (height(node.left) - height(node.right) < 1) {
+            // right heavy
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // right left case
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
+
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+
+    public Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        p.left = t;
+        c.right = p;
+        p.height = Math.max(height(p.left), height(p.right)) + 1;
+        c.height = Math.max(height(c.left), height(c.right)) + 1;
+        return c;
+    }
+
+    public Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
+
+        c.right = t;
+        p.left = c;
+
+        c.height = Math.max(height(c.left), height(c.right)) + 1;
+        p.height = Math.max(height(p.left), height(p.right)) + 1;
+        return p;
+    }
+
     public void inOrder() {
         inOrder(root);
     }
@@ -72,6 +124,9 @@ public class AVL {
         tree.insert(11);
         tree.insert(12);
         tree.insert(13);
+        tree.insert(2);
+        tree.insert(22);
+        tree.insert(100);
         tree.inOrder();
     }
 }
