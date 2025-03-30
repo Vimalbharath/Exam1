08d8c12c4a125ecd3202b9924322f499bea5e936
Vimalbharath <vimalbharath21@gmail.com>
2025-03-30 20:11:30 +0530

    list of linkedlist

--- Graph/AdjacencyList.java
+++ Graph/AdjacencyList.java
@@ -1,19 +1,46 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class AdjacencyList {
-    LinkedList<Integer> list = new LinkedList<Integer>();
-    LinkedList<Integer> graph[];
+
+    List<LinkedList<Integer>> graph;
+    public boolean directed;
+
+    public AdjacencyList(int noofvertices, boolean directed) {
+        graph = new ArrayList<LinkedList<Integer>>(noofvertices);
+        this.directed = directed;
+    }
+
+    public void addedge(int u, int v) {
+        addedge(graph, u, v);
+    }
+
+    public void addedge(List<LinkedList<Integer>> graph, int u, int v) {
+        graph.get(u).add(v);
+        if (!directed) {
+            graph.get(v).add(u);
+        }
+    }
+
+    public void printgraph() {
+        System.out.println("Printing adjacency list");
+        for (LinkedList<Integer> list : graph) {
+            for (int val : list) {
+                System.out.print(" " + val + " ");
+            }
+            System.out.println();
+
+        }
+    }
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        LinkedList<Integer>[] graph;
-        graph[0] = new LinkedList<Integer>();
-        for (int i = 0; i < 5; i++) {
-            graph[i] = new LinkedList<Integer>();
-        }
-
+        AdjacencyList graph = new AdjacencyList(5, false);
+        graph.addedge(2, 4);
+        graph.addedge(2, 1);
+        graph.printgraph();
     }
 
 }
