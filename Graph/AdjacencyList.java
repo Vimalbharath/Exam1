00979f9b9730cc584f91fabead144a346671fa77
Vimalbharath <vimalbharath21@gmail.com>
2025-03-30 19:49:40 +0530

    array of linked list??

---
+++ Graph/AdjacencyList.java
@@ -0,0 +1,19 @@
+package Graph;
+
+import java.util.LinkedList;
+
+public class AdjacencyList {
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    LinkedList<Integer> graph[];
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        LinkedList<Integer>[] graph;
+        graph[0] = new LinkedList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            graph[i] = new LinkedList<Integer>();
+        }
+
+    }
+
+}
