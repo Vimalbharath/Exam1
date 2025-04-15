cb874062598091571959b2f472d34bc9e6755ffd
Vimalbharath <vimalbharath21@gmail.com>
2025-04-15 23:35:28 +0530

    wrong ans

--- Prefixsum/pivotindex.java
+++ Prefixsum/pivotindex.java
@@ -2,16 +2,16 @@ package Prefixsum;
 
 public class pivotindex {
     public int pivotIndex(int[] nums) {
-        int ans=-1;
-        int size=nums.length;
-        int prefixsum[]=new int[size-1];
-        int suffixsum[]new int[size-1];
-        prefixsum[0]=nums[0];
-        suffixsum[size-1]=nums[size-1];
-
-        for(int i=1;i<nums.length;i++){
-            prefixsum[i]=prefixsum[i-1]+nums[i];
-            suffixsum[size-i-1]=0;
+        int ans = -1;
+        int prefixsum = 0;
+        int suffixsum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefixsum = prefixsum + nums[i];
+            suffixsum = suffixsum + nums[nums.length - i - 1];
+            if (prefixsum == suffixsum) {
+                ans = i;
+            }
         }
+        return ans;
     }
 }
