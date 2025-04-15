5afb11ccd3b494c1f944b8b98ecfb9b0a30f2013
Vimalbharath <vimalbharath21@gmail.com>
2025-04-15 23:52:04 +0530

    test case fail

--- Prefixsum/pivotindex.java
+++ Prefixsum/pivotindex.java
@@ -4,8 +4,8 @@ public class pivotindex {
     public int pivotIndex(int[] nums) {
         int ans = -1;
         int size = nums.length;
-        int prefixsum[] = new int[size - 1];
-        int suffixsum[] = new int[size - 1];
+        int prefixsum[] = new int[size];
+        int suffixsum[] = new int[size];
         prefixsum[0] = nums[0];
         suffixsum[size - 1] = nums[size - 1];
 
@@ -13,7 +13,7 @@ public class pivotindex {
             prefixsum[i] = prefixsum[i - 1] + nums[i];
         }
 
-        for (int i = size - 2; i > 0; i--) {
+        for (int i = size - 2; i >= 0; i--) {
             suffixsum[i] = suffixsum[i + 1] + nums[i];
         }
 
