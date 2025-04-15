e4c94ce0522d0a0a101e78a24b70a803729dcabc
Vimalbharath <vimalbharath21@gmail.com>
2025-04-15 23:43:59 +0530

    light ans

--- Prefixsum/pivotindex.java
+++ Prefixsum/pivotindex.java
@@ -3,15 +3,27 @@ package Prefixsum;
 public class pivotindex {
     public int pivotIndex(int[] nums) {
         int ans = -1;
-        int prefixsum = 0;
-        int suffixsum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            prefixsum = prefixsum + nums[i];
-            suffixsum = suffixsum + nums[nums.length - i - 1];
-            if (prefixsum == suffixsum) {
+        int size = nums.length;
+        int prefixsum[] = new int[size - 1];
+        int suffixsum[] = new int[size - 1];
+        prefixsum[0] = nums[0];
+        suffixsum[size - 1] = nums[size - 1];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefixsum[i] = prefixsum[i - 1] + nums[i];
+        }
+
+        for (int i = size - 2; i > 0; i--) {
+            suffixsum[i] = suffixsum[i + 1] + nums[i];
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (prefixsum[i] == suffixsum[i]) {
                 ans = i;
             }
+
         }
+
         return ans;
     }
 }
