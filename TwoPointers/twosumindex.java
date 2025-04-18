eea114e6b3243261b825a78353eb5e5f4d631cd0
Vimalbharath <vimalbharath21@gmail.com>
2025-04-18 14:43:57 +0530

    only by hash unsorted

--- TwoPointers/twosumindex.java
+++ TwoPointers/twosumindex.java
@@ -3,6 +3,16 @@ package TwoPointers;
 import java.util.Arrays;
 
 public class twosumindex {
+    public int[] twoSumhash(int[] a, int t) {
+        var m = new java.util.HashMap<Integer, Integer>();
+        for (int i = 0; i < a.length; i++) {
+            if (m.containsKey(t - a[i]))
+                return new int[] { m.get(t - a[i]), i };
+            m.put(a[i], i);
+        }
+        return null;
+    }
+
     public int[] twoSum(int[] nums, int target) {
         Arrays.sort(nums);
         int left = 0;
@@ -15,6 +25,7 @@ public class twosumindex {
                 ans[0] = left;
                 ans[1] = right;
                 return ans;
+                // return new int[] {left,right};
             }
             if (sum > target) {
                 right--;
