933f46d3d410e8237b9099d119846bb19d08e393
Vimalbharath <vimalbharath21@gmail.com>
2025-04-19 14:03:33 +0530

    arraylist

--- TwoPointers/pairlessthantarget.java
+++ TwoPointers/pairlessthantarget.java
@@ -1,18 +1,18 @@
 package TwoPointers;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class pairlessthantarget {
 
     public int countPairs(List<Integer> nums, int target) {
         int ans = 0;
-        for (int num : nums) {
-            int val = target - num;
-            for (int snum : nums) {
-                if (snum < val) {
-                    if (snum == num) {
-                        ans--;
-                    }
+        for (int i = 0; i < nums.size(); i++) {
+
+            for (int j = 0; j < nums.size(); j++) {
+
+                if (i < j && (nums.get(i) + nums.get(j)) < target) {
+
                     ans++;
                 }
             }
