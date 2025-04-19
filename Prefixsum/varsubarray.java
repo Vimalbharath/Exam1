21a6d3ee6220441d512083381d4fd1fd8fa02b14
Vimalbharath <vimalbharath21@gmail.com>
2025-04-20 00:07:05 +0530

    like range sum copy

--- Prefixsum/varsubarray.java
+++ Prefixsum/varsubarray.java
@@ -3,6 +3,16 @@ package Prefixsum;
 //https://leetcode.com/problems/sum-of-variable-length-subarrays/description/?envType=problem-list-v2&envId=prefix-sum
 
 public class varsubarray {
+    public int subarraySum1(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int j = i - nums[i] - 1;
+            nums[i] += nums[i - 1];
+            res += nums[i] - (j < 0 ? 0 : nums[j]);
+        }
+        return res;
+    }
+
     public int subarraySum(int[] nums) {
         int ans = 0;
         for (int i = 0; i < nums.length; i++) {
