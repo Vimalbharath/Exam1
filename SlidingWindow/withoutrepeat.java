92c9e00d6fa8309669fc2a59d45feceef0e8c22e
Vimalbharath <vimalbharath21@gmail.com>
2025-04-20 21:25:36 +0530

    too good

---
+++ SlidingWindow/withoutrepeat.java
@@ -0,0 +1,46 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class withoutrepeat {
+    public static int longestUniqueSubstr(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        int maxarrl = 0;
+        int arrl = 0;
+        while (j < n) {
+            char ch = s.charAt(j);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            int count = map.size();
+
+            if (count < j - i + 1) {
+                char cha = s.charAt(i);
+                if (map.get(cha) == 1) {
+                    map.remove(cha);
+                } else {
+                    map.put(cha, map.get(cha) - 1);
+                }
+                i++;
+                // j++;
+            }
+            if (count == j - i + 1) {
+                arrl = j - i + 1;
+                if (arrl > maxarrl) {
+                    maxarrl = arrl;
+                }
+                j++;
+            }
+
+        }
+        return maxarrl;
+    }
+
+    public static void main(String[] args) {
+        String s = "asdfghjkloiuytrewqzxcvbnm";
+        System.out.println(longestUniqueSubstr(s));
+    }
+}
