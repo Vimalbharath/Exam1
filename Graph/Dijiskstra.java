85da0fe3290c7a44c69601acdf69c4dae45b0a70
Vimalbharath <vimalbharath21@gmail.com>
2025-04-26 14:27:31 +0530

    logic wrong

--- Graph/Dijiskstra.java
+++ Graph/Dijiskstra.java
@@ -1,20 +1,28 @@
 package Graph;
 
+import java.util.Arrays;
+
 public class Dijiskstra {
 
-    public int[] dmatrix(int[][] matrix) {
+    public static int[] dmatrix(int[][] matrix) {
         int[] dist = new int[4];
-        for (int element : dist) {
-            element = Integer.MAX_VALUE;
+        // for (int val : dist) {
+        // val = Integer.MAX_VALUE;
+        // }
+        for (int i = 0; i < dist.length; i++) {
+            dist[i] = Integer.MAX_VALUE;
         }
         dist[0] = 0;
 
-        int j = 1;
-        for (int i = 0; i < matrix.length; i++) {
-            if (dist[i] > matrix[j][i] + dist[j]) {
-                dist[i] = matrix[j][i] + dist[j];
+        for (int j = 0; j < matrix.length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                if (dist[i] > matrix[j][i] + dist[j]) {
+                    dist[i] = matrix[j][i] + dist[j];
+                }
             }
         }
+
+        return dist;
     }
 
     public static void main(String[] args) {
@@ -24,6 +32,6 @@ public class Dijiskstra {
                 { 3, 0, 0, 0 },
                 { 4, 5, 0, 0 }
         };
-        System.out.println(dmatrix(matrix));
+        System.out.println(Arrays.toString(dmatrix(matrix)));
     }
 }
