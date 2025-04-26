e8a781ec0c77d8e0cca8988701a1cef3b8198f14
Vimalbharath <vimalbharath21@gmail.com>
2025-04-26 14:52:47 +0530

    failed to check matrix[u][v] != 0

--- Graph/Dijiskstra.java
+++ Graph/Dijiskstra.java
@@ -6,18 +6,22 @@ public class Dijiskstra {
 
     public static int[] dmatrix(int[][] matrix) {
         int[] dist = new int[4];
+        boolean[] visited = new boolean[4];
         // for (int val : dist) {
         // val = Integer.MAX_VALUE;
         // }
         for (int i = 0; i < dist.length; i++) {
             dist[i] = Integer.MAX_VALUE;
+            visited[i] = false;
         }
         dist[0] = 0;
+        visited[0] = true;
 
-        for (int j = 0; j < matrix.length; j++) {
-            for (int i = 0; i < matrix.length; i++) {
-                if (dist[i] > matrix[j][i] + dist[j]) {
-                    dist[i] = matrix[j][i] + dist[j];
+        for (int u = 0; u < matrix.length; u++) {
+            for (int v = 0; v < matrix.length; v++) {
+                if (!visited[v] && matrix[u][v] != 0 && dist[u] != Integer.MAX_VALUE
+                        && dist[v] > matrix[u][v] + dist[u]) {
+                    dist[v] = matrix[u][v] + dist[u];
                 }
             }
         }
