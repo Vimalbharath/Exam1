c32f546b1b75acea337848cd749cd4def38e242a
Vimalbharath <vimalbharath21@gmail.com>
2025-04-27 17:43:40 +0530

    great

--- Graph/topomatrix.java
+++ Graph/topomatrix.java
@@ -1,27 +1,33 @@
 package Graph;
 
+import java.util.Stack;
+
 public class topomatrix {
 
     public static void dfs(int[][] graph) {
         int[] visited = new int[graph.length];
+        Stack<Integer> st = new Stack<>();
         for (int i = 0; i < graph.length; i++) {
             if (visited[i] == 0) {
-                dfs(graph, i, visited);
-                System.out.println(i);
+                dfs(graph, i, visited, st);
+                st.push(i);
             }
 
+        }
+        while (!st.empty()) {
+            System.out.println(st.pop());
         }
 
     }
 
-    public static void dfs(int[][] graph, int start, int[] visited) {
+    public static void dfs(int[][] graph, int start, int[] visited, Stack<Integer> st) {
         visited[start] = 1;
         // System.out.println(start);
         for (int i = 0; i < graph.length; i++) {
             int v = graph[start][i];
             if (visited[i] == 0 && v == 1) {
-                dfs(graph, i, visited);
-                System.out.println(i);
+                dfs(graph, i, visited, st);
+                st.push(i);
             }
 
         }
