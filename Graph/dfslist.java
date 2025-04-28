f0e95d81bc42740381543191f65b6889e476fe94
Vimalbharath <vimalbharath21@gmail.com>
2025-04-28 13:18:32 +0530

    time not updating properly

--- Graph/dfslist.java
+++ Graph/dfslist.java
@@ -1,6 +1,7 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class dfslist {
     public static void addEdge(ArrayList<ArrayList<Integer>> adj, int s,
@@ -11,26 +12,36 @@ public class dfslist {
 
     public static void dfs(ArrayList<ArrayList<Integer>> adj) {
         int[] visited = new int[adj.size()];
+        int[] arrival = new int[adj.size()];
+        int[] depature = new int[adj.size()];
+        int time = 0;
         for (int i = 0; i < adj.size(); i++) {
             if (visited[i] == 0) {
                 // visited[i] = 1;
-                dfs(adj, visited, i);
+                dfs(adj, visited, i, arrival, depature, time);
 
             }
         }
+        System.out.println(Arrays.toString(arrival));
+        System.out.println(Arrays.toString(depature));
 
     }
 
-    public static void dfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int i) {
+    public static void dfs(ArrayList<ArrayList<Integer>> adj, int[] visited, int i, int[] arrival, int[] depature,
+            int time) {
         visited[i] = 1;
+        time = time + 1;
+        arrival[i] = time;
         System.out.println(i);
         for (int j = 0; j < adj.get(i).size(); j++) {
             int v = adj.get(i).get(j);
             if (visited[v] == 0) {
                 // visited[v] = 1;
-                dfs(adj, visited, v);
+                dfs(adj, visited, v, arrival, depature, time);
             }
         }
+        time = time + 1;
+        depature[i] = time;
 
     }
 
