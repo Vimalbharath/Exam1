a7bf531e9425b082bf449c94d42055ef3b9274d5
Vimalbharath <vimalbharath21@gmail.com>
2025-04-30 13:30:32 +0530

    floydWarshall

---
+++ Graph/Floyd.java
@@ -0,0 +1,36 @@
+package Graph;
+
+public class Floyd {
+
+    public static void floydWarshall(int[][] dist) {
+        int size = dist.length;
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int INF = 100000000;
+
+        int[][] dist = { { 0, 4, INF, 5, INF },
+                { INF, 0, 1, INF, 6 },
+                { 2, INF, 0, 3, INF },
+                { INF, INF, 1, 0, 2 },
+                { 1, INF, INF, 4, 0 } };
+
+        floydWarshall(dist);
+        for (int i = 0; i < dist.length; i++) {
+            for (int j = 0; j < dist.length; j++) {
+                System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
