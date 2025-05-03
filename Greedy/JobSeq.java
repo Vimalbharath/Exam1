0498fd40676453414e5bf8b0f497d73a456ecccd
Vimalbharath <vimalbharath21@gmail.com>
2025-05-03 20:37:13 +0530

    tried coding naive ans wrong

--- Greedy/JobSeq.java
+++ Greedy/JobSeq.java
@@ -5,7 +5,32 @@ import java.util.ArrayList;
 public class JobSeq {
     public static ArrayList<Integer> jobSequencing(int[] deadline, int[] profit) {
         ArrayList<Integer> ans = new ArrayList<>();
-        ArrayList<ArrayList<Integer>>=new ArrayList<>();
+        int n = deadline.length;
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>(2));
+            list.get(i).add(profit[i]);
+            list.get(i).add(deadline[i]);
+        }
+
+        list.sort((a, b) -> Integer.compare(a.get(0), b.get(0)));
+        int start = -1;
+        int totprofit = 0;
+        int count = 0;
+        int[] slot = new int[n];
+        for (int i = 0; i < n; i++) {
+            start = list.get(i).get(1);
+            for (int j = start; j >= 0; j--) {
+                if (slot[j] == 0) {
+                    slot[j] = 1;
+                    totprofit += list.get(i).get(0);
+                    count++;
+                }
+            }
+
+        }
+        ans.add(totprofit);
+        ans.add(count);
         return ans;
 
     }
