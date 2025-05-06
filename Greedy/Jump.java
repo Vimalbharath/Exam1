2ed00cfb1a0d736d6ad27541a8ae60c29e4f64eb
Vimalbharath <vimalbharath21@gmail.com>
2025-05-06 13:59:04 +0530

    try on own

---
+++ Greedy/Jump.java
@@ -0,0 +1,22 @@
+package Greedy;
+
+public class Jump {
+    public boolean canJump(int[] nums) {
+        int currentreach = 0;
+        int n = nums.length;
+        int maxreach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            maxreach = Math.min(i + nums[i], maxreach);
+            if (maxreach > n - 1)
+                return true;
+            if (i == currentreach) {
+                if (i == maxreach) {
+                    return false;
+                } else {
+                    currentreach = maxreach;
+                }
+            }
+        }
+        return true;
+    }
+}
