074772f3315f7324f2d3777c8bbde257f11cd79b
Vimalbharath <vimalbharath21@gmail.com>
2025-05-08 22:46:53 +0530

    leetcode variation

--- DP/zeroone/SubsetSum.java
+++ DP/zeroone/SubsetSum.java
@@ -1,25 +1,26 @@
 package DP.zeroone;
 
+//https://leetcode.com/problems/partition-equal-subset-sum/description/
+
 public class SubsetSum {
     public static int perfectSum(int[] arr, int target) {
-        return perfectSum(0, 0, arr, target);
+        return perfectSum(target, 0, arr, target);
 
     }
 
     public static int perfectSum(int currsum, int i, int[] arr, int target) {
 
-        if (i == arr.length) {
-            return 0;
-        }
-
         if (currsum == 0) {
             return 1;
+        }
+        if (i == arr.length) {
+            return 0;
         }
 
         if (currsum < 0) {
             return 0;
         }
-        int left = perfectSum(target - arr[i], i + 1, arr, target);
+        int left = perfectSum(currsum - arr[i], i + 1, arr, target);
         int right = perfectSum(currsum, i + 1, arr, target);
         return left + right;
     }
