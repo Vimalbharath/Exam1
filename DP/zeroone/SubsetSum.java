dc7b58239f28e7ea4fd2264e1688af6541848819
Vimalbharath <vimalbharath21@gmail.com>
2025-05-08 21:53:13 +0530

    wrong atleast answer

---
+++ DP/zeroone/SubsetSum.java
@@ -0,0 +1,26 @@
+package DP.zeroone;
+
+public class SubsetSum {
+    public static int perfectSum(int[] arr, int target) {
+        return perfectSum(0, 0, arr, target);
+
+    }
+
+    public static int perfectSum(int count, int i, int[] arr, int target) {
+
+        if (target == 0) {
+            return 1;
+        }
+
+        if (target > 0) {
+            count = count + perfectSum(count, i + 1, arr, target - arr[i]);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 3 };
+        int target = 6;
+        System.out.println(perfectSum(arr, target));
+    }
+}
