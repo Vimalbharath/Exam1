d8e1cf846cf2d2858feebe227739858794ea5429
Vimalbharath <vimalbharath21@gmail.com>
2025-05-09 22:48:23 +0530

    tried dp confused on boolean int

--- DP/zeroone/EqualSum.java
+++ DP/zeroone/EqualSum.java
@@ -1,5 +1,7 @@
 package DP.zeroone;
 
+import java.util.Arrays;
+
 public class EqualSum {
 
     public boolean canPartition(int[] nums) {
@@ -11,16 +13,35 @@ public class EqualSum {
         if (sum % 2 == 1) {
             return false;
         }
-        return canSum(nums, sum / 2, n - 1);
+        int[][] memo = new int[n][sum + 1];
+        for (int[] a : memo) {
+            Arrays.fill(a, -1);
+        }
+
+        return canSum(nums, sum / 2, n - 1, memo);
     }
 
-    public boolean canSum(int[] nums, int sum, int n) {
+    public boolean canSum(int[] nums, int sum, int n, int[][] memo) {
+        if (memo[n][sum] != -1) {
+            return (memo[n][sum] == 1 ? true : false);
+        }
         if (sum == 0) {
             return true;
         }
         if (n == 0) {
             return false;
         }
-        return canSum(nums, sum - nums[n], n - 1) || canSum(nums, sum, n - 1);
+
+        if (canSum(nums, sum - nums[n], n - 1, memo)) {
+            memo[n - 1][sum - nums[n]] = 1;
+        } else {
+            memo[n - 1][sum - nums[n]] = 0;
+        }
+        if (canSum(nums, sum, n - 1, memo)) {
+            memo[n - 1][sum] = 1;
+        } else {
+            memo[n - 1][sum] = 0;
+        }
+        return canSum(nums, sum - nums[n], n - 1, memo) || canSum(nums, sum, n - 1, memo);
     }
 }
