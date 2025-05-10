d1c960a1dc9e42ecfd9b0bb3ce45d71059120cf8
Vimalbharath <vimalbharath21@gmail.com>
2025-05-10 21:38:25 +0530

    tried count sum table

--- DP/zeroone/SubsetSum.java
+++ DP/zeroone/SubsetSum.java
@@ -3,6 +3,29 @@ package DP.zeroone;
 //https://leetcode.com/problems/partition-equal-subset-sum/description/
 
 public class SubsetSum {
+    public static int perfectSum2(int[] arr, int target) {
+        int[][] memo = new int[arr.length + 1][target + 1];
+        // for (int i = 0; i <= target; i++) {
+        // memo[0][i] = 0;
+        // }
+        for (int i = 0; i <= arr.length; i++) {
+            memo[0][i] = 1;
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (arr[i - 1] <= j) {
+                    memo[i][j] = memo[i - 1][j - arr[i]];
+                } else {
+                    memo[i][j] = memo[i - 1][j];
+                }
+            }
+        }
+
+        return memo[arr.length][target];
+
+    }
+
     public static int perfectSum(int[] arr, int target) {
         return perfectSum(target, 0, arr, target);
 
@@ -28,6 +51,6 @@ public class SubsetSum {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 3 };
         int target = 6;
-        System.out.println(perfectSum(arr, target));
+        System.out.println(perfectSum2(arr, target));
     }
 }
