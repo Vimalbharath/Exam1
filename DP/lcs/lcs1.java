7e27ba6d8811c7023525f4fe838611399829d74f
Vimalbharath <vimalbharath21@gmail.com>
2025-05-11 20:49:57 +0530

    video corrected code

--- DP/lcs/lcs1.java
+++ DP/lcs/lcs1.java
@@ -2,14 +2,14 @@ package DP.lcs;
 
 public class lcs1 {
     public static int longestCommonSubsequence(String text1, String text2) {
-        return longestCommonSubsequence(text1, text2, text1.length() - 1, text2.length() - 1);
+        return longestCommonSubsequence(text1, text2, text1.length(), text2.length());
     }
 
     public static int longestCommonSubsequence(String text1, String text2, int l1, int l2) {
         if (l1 == 0 || l2 == 0) {
             return 0;
         }
-        if (text1.charAt(l1) == text2.charAt(l2)) {
+        if (text1.charAt(l1 - 1) == text2.charAt(l2 - 1)) {
             return 1 + longestCommonSubsequence(text1, text2, l1 - 1, l2 - 1);
         } else {
             return Math.max(longestCommonSubsequence(text1, text2, l1, l2 - 1),
