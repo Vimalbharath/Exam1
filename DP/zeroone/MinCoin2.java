4fde5b1e8c498d21c394bfcde79bc14781ed00c4
Vimalbharath <vimalbharath21@gmail.com>
2025-05-11 19:48:07 +0530

    answer perfect

--- DP/zeroone/MinCoin2.java
+++ DP/zeroone/MinCoin2.java
@@ -7,8 +7,8 @@ public class MinCoin2 {
             dp[0][i] = Integer.MAX_VALUE - 1;
         }
         for (int i = 1; i <= amt; i++) {
-            if (amt % i == 0) {
-                dp[1][i] = amt / i;
+            if (i % coins[0] == 0) {
+                dp[1][i] = i / coins[0];
             } else {
                 dp[1][i] = Integer.MAX_VALUE - 1;
             }
@@ -16,7 +16,7 @@ public class MinCoin2 {
         for (int i = 2; i <= coins.length; i++) {
             for (int j = 1; j <= amt; j++) {
                 if (coins[i - 1] <= j) {
-                    dp[i][j] = Math.min(dp[i - 1][j - coins[i - 1]], dp[i - 1][j]);
+                    dp[i][j] = Math.min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
@@ -32,7 +32,7 @@ public class MinCoin2 {
     }
 
     public static void main(String[] args) {
-        int[] coins = { 1, 2, 5 };
+        int[] coins = { 1, 2, 3 };
         int amt = 5;
         System.out.println(count(amt, coins));
     }
