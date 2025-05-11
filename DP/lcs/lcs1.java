42d06c69a52b94cc9e34aed7a5a651179734f964
Vimalbharath <vimalbharath21@gmail.com>
2025-05-11 20:56:31 +0530

    dp top down memo in rec success

--- DP/lcs/lcs1.java
+++ DP/lcs/lcs1.java
@@ -1,19 +1,28 @@
 package DP.lcs;
 
+import java.util.Arrays;
+
 public class lcs1 {
     public static int longestCommonSubsequence(String text1, String text2) {
-        return longestCommonSubsequence(text1, text2, text1.length(), text2.length());
+        int memo[][] = new int[text1.length() + 1][text2.length() + 1];
+        for (int[] a : memo) {
+            Arrays.fill(a, -1);
+        }
+        return longestCommonSubsequence(text1, text2, text1.length(), text2.length(), memo);
     }
 
-    public static int longestCommonSubsequence(String text1, String text2, int l1, int l2) {
+    public static int longestCommonSubsequence(String text1, String text2, int l1, int l2, int[][] memo) {
+        if (memo[l1][l2] != -1) {
+            return memo[l1][l2];
+        }
         if (l1 == 0 || l2 == 0) {
-            return 0;
+            return memo[l1][l2] = 0;
         }
         if (text1.charAt(l1 - 1) == text2.charAt(l2 - 1)) {
-            return 1 + longestCommonSubsequence(text1, text2, l1 - 1, l2 - 1);
+            return memo[l1][l2] = 1 + longestCommonSubsequence(text1, text2, l1 - 1, l2 - 1, memo);
         } else {
-            return Math.max(longestCommonSubsequence(text1, text2, l1, l2 - 1),
-                    longestCommonSubsequence(text1, text2, l1 - 1, l2));
+            return memo[l1][l2] = Math.max(longestCommonSubsequence(text1, text2, l1, l2 - 1, memo),
+                    longestCommonSubsequence(text1, text2, l1 - 1, l2, memo));
         }
     }
 
