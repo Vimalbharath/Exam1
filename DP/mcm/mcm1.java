526adbbca0c0d9a36436e8c085521087a324fec1
Vimalbharath <vimalbharath21@gmail.com>
2025-05-13 23:03:24 +0530

    answer correct

--- DP/mcm/mcm1.java
+++ DP/mcm/mcm1.java
@@ -6,9 +6,9 @@ public class mcm1 {
             return 0;
         }
         int ans = Integer.MAX_VALUE;
-        for (int k = i + 1; k <= j - 1; j++) {
+        for (int k = i; k <= j - 1; k++) {
             int temp = matrixMultiplication(arr, i, k) + matrixMultiplication(arr, k + 1, j) +
-                    (arr[i] * arr[k] * arr[j]);
+                    (arr[i - 1] * arr[k] * arr[j]);
             if (temp < ans) {
                 ans = temp;
             }
@@ -19,6 +19,6 @@ public class mcm1 {
 
     public static void main(String[] args) {
         int[] arr = { 2, 1, 3, 4 };
-        System.out.println(matrixMultiplication(arr, 0, arr.length - 1));
+        System.out.println(matrixMultiplication(arr, 1, arr.length - 1));
     }
 }
