e999e95b44e870074fadc56a8199f61f05364bde
Vimalbharath <vimalbharath21@gmail.com>
2025-05-18 16:50:01 +0530

    optimize memo try

--- DP/mcm/eggdrop.java
+++ DP/mcm/eggdrop.java
@@ -1,8 +1,13 @@
 package DP.mcm;
 
-public class eggdrop {
-    public static int minattempt(int e, int f) {
+import java.util.Arrays;
 
+public class eggdrop {
+    public static int minattempt(int e, int f, int[][] dp) {
+
+        if (dp[e][f] != -1) {
+            return dp[e][f];
+        }
         if (e == 1) {
             return f;
         }
@@ -12,15 +17,29 @@ public class eggdrop {
 
         int attempt = Integer.MAX_VALUE;
         for (int k = 1; k <= f; k++) {
-            int ans = 1 + Math.max(minattempt(e - 1, k - 1), minattempt(e, f - k));
+            int left = dp[e - 1][k - 1];
+            if (dp[e - 1][k - 1] == -1) {
+                left = minattempt(e - 1, k - 1, dp);
+                dp[e - 1][k - 1] = left;
+            }
+            int right = dp[e][f - k];
+            if (dp[e][f - k] == -1) {
+                left = minattempt(e, f - k, dp);
+                dp[e][f - k] = right;
+            }
+            int ans = 1 + Math.max(left, right);
             attempt = Math.min(attempt, ans);
         }
-        return attempt;
+        return dp[e][f] = attempt;
     }
 
     public static void main(String[] args) {
         int e = 2;
-        int f = 10;
-        System.out.println(minattempt(e, f));
+        int f = 100;
+        int[][] dp = new int[e + 1][f + 1];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        System.out.println(minattempt(e, f, dp));
     }
 }
