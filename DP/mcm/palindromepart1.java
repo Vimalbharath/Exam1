e42bfee8e683bd7c5ecd3782174f2ea342009f04
Vimalbharath <vimalbharath21@gmail.com>
2025-05-18 08:16:21 +0530

    not optimised but dp

--- DP/mcm/palindromepart1.java
+++ DP/mcm/palindromepart1.java
@@ -1,5 +1,7 @@
 package DP.mcm;
 
+import java.util.Arrays;
+
 public class palindromepart1 {
     public static boolean isPalindrome(String str, int i, int j) {
         if (i == j) {
@@ -20,24 +22,38 @@ public class palindromepart1 {
 
     }
 
-    public static int pp(String str, int s, int e) {
+    public static int pp(String str, int s, int e, int[][] dp) {
+        if (dp[s][e] != -1) {
+            return dp[s][e];
+        }
         if (s >= e) {
-            return 0;
+            return dp[s][e] = 0;
         }
         if (isPalindrome(str, s, e) == true) {
-            return 0;
+            return dp[s][e] = 0;
         }
         int ans = Integer.MAX_VALUE;
         for (int k = s; k <= e - 1; k++) {
-            int temp = pp(str, s, k) + 1 + pp(str, k + 1, e);
+            int temp = pp(str, s, k, dp) + 1 + pp(str, k + 1, e, dp);
             ans = Math.min(temp, ans);
         }
-        return ans;
+        return dp[s][e] = ans;
     }
 
     public static void main(String[] args) {
         String a = "nitim";
-        System.out.println(pp(a, 0, a.length() - 1));
+        int[][] dp = new int[a.length() + 1][a.length() + 1];
+        for (int[] b : dp) {
+            Arrays.fill(b, -1);
+        }
+        System.out.println(pp(a, 0, a.length() - 1, dp));
         System.out.println(isPalindrome(a, 0, 4));
+
+        for (int[] c : dp) {
+            for (int d : c) {
+                System.out.print(d + " ");
+            }
+            System.out.println();
+        }
     }
 }
