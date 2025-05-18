5fbefc43a9ea1f43d7657c4c524bb796fbdadacc
Vimalbharath <vimalbharath21@gmail.com>
2025-05-18 16:34:32 +0530

    small change base

--- DP/mcm/eggdrop.java
+++ DP/mcm/eggdrop.java
@@ -2,9 +2,14 @@ package DP.mcm;
 
 public class eggdrop {
     public static int minattempt(int e, int f) {
-        if (e <= 1 || f <= 0) {
+
+        if (e == 1) {
             return f;
         }
+        if (f == 1 || f == 0) {
+            return f;
+        }
+
         int attempt = Integer.MAX_VALUE;
         for (int k = 1; k <= f; k++) {
             int ans = 1 + Math.max(minattempt(e - 1, k - 1), minattempt(e, f - k));
@@ -14,8 +19,8 @@ public class eggdrop {
     }
 
     public static void main(String[] args) {
-        int e = 3;
-        int f = 5;
+        int e = 2;
+        int f = 10;
         System.out.println(minattempt(e, f));
     }
 }
